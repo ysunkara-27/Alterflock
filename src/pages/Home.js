2b0b7cbc11c0d8fc88728f36a78fb19ed9678441
Yash <sunkarayashaswi@gmail.com>
2024-06-05 15:49:59 -0400

    useState and Effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Element } from 'react-scroll';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
