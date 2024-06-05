7b5ed1989f3f690ba86e8c777e94d63b6547c256
Yash <sunkarayashaswi@gmail.com>
2024-06-05 15:46:38 -0400

    useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { Element } from 'react-scroll';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
